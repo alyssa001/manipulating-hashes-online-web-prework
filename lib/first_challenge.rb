64d1e67db88a69b09c9013f54d5c5ed629030f50
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-06-03 13:50:53 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,8 @@ def first_challenge
   contacts["Freddy Mercury"][:favorite_icecream_flavors]
   contacts.each do |person, data|
     data.each do |attribute, value|
-      
+      if attribute 
+        
 
   
   contacts
